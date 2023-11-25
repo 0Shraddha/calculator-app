75f88daa2039a8a9f835faee87e45eff019b3038
0Shraddha <shraddhadongol12345@gmail.com>
2023-11-25 19:52:52 +0545

    initial commit

---
+++ src/components/Operations/Index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import "./Index.css";
+
+const Index = () => {
+  return (
+    <>
+    <div className='heading'>Simple Calculator</div>
+    <div className='calculator-main'>
+      <div className='numbers-signs'>
+        <div className='grid-item item1'>AC</div>
+        <div className='grid-item item2'>/</div>
+        <div className='grid-item item3'>*</div>
+        <div className='grid-item item4'>9</div>
+        <div className='grid-item item4'>8</div>
+        <div className='grid-item item4'>7</div>
+        <div className='grid-item item4'>-</div>
+        <div className='grid-item item4'>6</div>
+        <div className='grid-item item4'>5</div>
+        <div className='grid-item item4'>4</div>
+        <div className='grid-item item4'>+</div>
+        <div className='grid-item item4'>3</div>
+        <div className='grid-item item4'>2</div>
+        <div className='grid-item item4'>1</div>
+        <div className='grid-item item4'>=</div>
+        <div className='grid-item item4'>B/W</div>
+        <div className='grid-item item4'>0</div>
+        <div className='grid-item item5'>.</div>
+        <div className='grid-item item5'>backspace</div>
+
+      </div>
+      
+    </div>
+    </>
+  )
+}
+
+export default Index
